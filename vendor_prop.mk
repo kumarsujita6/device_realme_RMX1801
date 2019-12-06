7b53c279f46edbdb4545584394dff85f6deeac7e
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:58:23 -0800

    RMX1801: Use 4GB dalvik-heap config
    We are currently using 2GB dalvik-heap config.
    
    Bug: 145823510
    Test: MPTS test
    Change-Id: Ie26b8e4fefea25fa818bd1eaf9df4a0bb153d42d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -50,15 +50,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.media.codec2=2
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=384m \
-    dalvik.vm.heapmaxfree=16m \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heaptargetutilization=0.75 \
-
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
