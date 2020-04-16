5af87e2a0fe80c7cbb0d194adda5eb1748520ea0
ragy <wad.moskla@gmail.com>
2020-04-16 12:22:26 +0200

    RMX1801: Include Google Camera Go Edition
    * At least better then Snap

--- device.mk
+++ device.mk
@@ -133,8 +133,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    libstdc++.vendor \
-    Snap
+    libgui_vendor \
+    GCam 
 
 # Charging
 PRODUCT_PACKAGES += \
